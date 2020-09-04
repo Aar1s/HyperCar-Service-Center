83cfab63e39c6cd4f8a9edb3c1952e7bc9267929
Aar1s <awolozhanin@gmail.com>
2020-09-04 21:18:43 +0500

    Practice with saving to memory

--- tickets/views.py
+++ tickets/views.py
@@ -5,6 +5,7 @@ from django.views.generic.base import TemplateView
 from collections import deque
 import datetime
 ticket_n = 0
+counter = 0
 waiting_time = 0
 
 
@@ -24,13 +25,14 @@ class MenuView(View):
         return render(request, 'menu/menu.html')
 
 
-class TicketView(MenuView):
+class TicketView(View):
     line_of_cars = {'Oil': {'Quantity': 0, 'ticket_numbers': []},
                 'Tires': {'Quantity': 0, 'ticket_numbers': []},
                 'Diagnostics': {'Quantity': 0, 'ticket_numbers': []}}
-    tickets = {}
+    tickets = {'tickets': []}
 
     def find_next_ticket(self):
+        global counter
         if self.line_of_cars['Oil']['Quantity'] > 0:
             next_ticket = self.line_of_cars['Oil']['ticket_numbers'][0]
         elif self.line_of_cars['Tires']['Quantity'] > 0:
@@ -39,7 +41,15 @@ class TicketView(MenuView):
             next_ticket = self.line_of_cars['Diagnostics']['ticket_numbers'][0]
         else:
             next_ticket = 0
-        return next_ticket
+        if counter == 0:
+            next_ticket = 1
+            counter += 1
+        self.tickets['tickets'].append(next_ticket)
+        if len(self.tickets['tickets']) < 3:
+            current_ticket = self.tickets['tickets'][-1]
+        else:
+            current_ticket = self.tickets['tickets'][-2]
+        return next_ticket, current_ticket
 
 
 class OilView(TicketView):
@@ -51,10 +61,7 @@ class OilView(TicketView):
         if ticket_n <= 2:
             waiting_time = 0
         else:
-            if super().line_of_cars['Oil']['Quantity'] == 2:
-                waiting_time = 2 * super().line_of_cars['Oil']['Quantity'] - 2
-            else:
-                waiting_time = 2 * super().line_of_cars['Oil']['Quantity']
+            waiting_time = 2 * (super().line_of_cars['Oil']['Quantity'])
         return render(request, 'get_ticket/ticket.html', context={
             'ticket_number': ticket_n,
             'waiting_time': waiting_time,
@@ -73,12 +80,8 @@ class TireView(TicketView):
         if ticket_n <= 2:
             waiting_time = 0
         else:
-            if super().line_of_cars['Tires']['Quantity'] == 2:
-                waiting_time = 2 * super().line_of_cars['Tires']['Quantity'] +\
-                               5 * super().line_of_cars['Tires']['Quantity'] - 5
-            else:
-                waiting_time = 2 * super().line_of_cars['Tires']['Quantity'] +\
-                               5 * super().line_of_cars['Tires']['Quantity']
+            waiting_time = 2 * (super().line_of_cars['Tires']['Quantity']) +\
+                           5 * (super().line_of_cars['Tires']['Quantity'])
 
         return render(request, 'get_ticket/ticket.html', context={
             'ticket_number': ticket_n,
@@ -97,17 +100,10 @@ class DiagView(TicketView):
         super().line_of_cars['Diagnostics']['ticket_numbers'].append(ticket_n)
         if ticket_n <= 2:
             waiting_time = 0
-        elif super().line_of_cars['Diagnostics']['Quantity'] == 1:
-            waiting_time = 2 * super().line_of_cars['Tires']['Quantity'] + 5 * super().line_of_cars['Tires']['Quantity']
         else:
-            if super().line_of_cars['Diagnostics']['Quantity'] == 2:
-                waiting_time = 2 * super().line_of_cars['Diagnostics']['Quantity'] +\
-                               5 * super().line_of_cars['Diagnostics']['Quantity'] +\
-                               30 * super().line_of_cars['Diagnostics']['Quantity'] - 30
-            else:
-                waiting_time = 2 * super().line_of_cars['Diagnostics']['Quantity'] +\
-                               5 * super().line_of_cars['Diagnostics']['Quantity'] +\
-                               30 * super().line_of_cars['Diagnostics']['Quantity']
+            waiting_time = 2 * super().line_of_cars['Tires']['Quantity'] +\
+                           5 * super().line_of_cars['Tires']['Quantity'] +\
+                           30 * (super().line_of_cars['Tires']['Quantity'])
         return render(request, 'get_ticket/ticket.html', context={
             'ticket_number': ticket_n,
             'waiting_time': waiting_time,
@@ -115,8 +111,6 @@ class DiagView(TicketView):
             'tires': super().line_of_cars['Tires']['Quantity'],
             'diagnostics': super().line_of_cars['Diagnostics']['Quantity']
         })
-
-# ВРЕМЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯ
 
 
 class OperatorView(TicketView):
@@ -130,27 +124,26 @@ class OperatorView(TicketView):
         })
 
     def post(self, request, *args, **kwargs):
-        if super().line_of_cars['Oil']['Quantity'] > 0:
-            super().line_of_cars['Oil']['Quantity'] -= 1
-            super().line_of_cars['Oil']['ticket_numbers'].pop(0)
-        elif super().line_of_cars['Tires']['Quantity'] > 0:
-            super().line_of_cars['Tires']['Quantity'] -= 1
-            super().line_of_cars['Tires']['ticket_numbers'].pop(0)
-        elif super().line_of_cars['Diagnostics']['Quantity'] > 0:
-            super().line_of_cars['Diagnostics']['Quantity'] -= 1
-            super().line_of_cars['Diagnostics']['ticket_numbers'].pop(0)
-        return redirect('/next/')
+        next_ticket = super().find_next_ticket()[0]
+        for key, value in super().line_of_cars.items():
+            if value['Quantity'] > 0:
+                for ticket in value['ticket_numbers']:
+                    if ticket == next_ticket:
+                        print(super().line_of_cars[key]['ticket_numbers'].pop(0))
+                        super().line_of_cars[key]['Quantity'] -= 1
+        return redirect('/')
 
 
 class NextView(TicketView):
     global ticket_n
 
     def get(self, request, *args, **kwargs):
+        next_ticket, current_ticket = super().find_next_ticket()
         if ticket_n > 2:
             busy = True
         else:
             busy = False
         return render(request, 'Next/next.html', context={
-            'ticket_number': self.find_next_ticket,
-            'busy': busy
+            'ticket_number': current_ticket,
+            'busy': busy,
         })
